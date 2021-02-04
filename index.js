b546faf6a699cacc41bcd7928d09a3c90506e3f2
Gina Lee <gina@thesistesting.com>
2021-02-04 02:51:47 -0800

    move initEmailInputListener() to own file

--- index.js
+++ index.js
@@ -1,18 +1,3 @@
-function validateEmail(email) {
-  const regex = /^(([^<>()[\]\\.,;:\s@\"]+(\.[^<>()[\]\\.,;:\s@\"]+)*)|(\".+\"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
-  return regex.test(email);
-};
-
 window.addEventListener('DOMContentLoaded', () => {
-  const emailCaptureForm = document.getElementById('email-capture-form');
-
-  emailCaptureForm.onsubmit = function(event) {
-    event.preventDefault();
-    const emailInput = event.target.querySelector('input');
-    const isValid = validateEmail(emailInput.value);
-
-    !isValid
-      ? event.target.classList.add('error')
-      : window.location.href = `./pages/loading.html?email=${emailInput.value}`;
-  };
+  initEmailInputListener('./pages/loading.html');
 });
---
+++ js/email_input.js
@@ -0,0 +1,18 @@
+function validateEmail(email) {
+  const regex = /^(([^<>()[\]\\.,;:\s@\"]+(\.[^<>()[\]\\.,;:\s@\"]+)*)|(\".+\"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+  return regex.test(email);
+};
+
+function initEmailInputListener(redirectUrl) {
+  const emailCaptureForm = document.getElementById('email-capture-form');
+  
+  emailCaptureForm.onsubmit = function(event) {
+    event.preventDefault();
+    const emailInput = event.target.querySelector('input');
+    const isValid = validateEmail(emailInput.value);
+  
+    !isValid
+      ? event.target.classList.add('error')
+      : window.location.href = `${redirectUrl}?email=${emailInput.value}`;
+  };
+}
